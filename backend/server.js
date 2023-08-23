f0535d23be194076a15a2069bc2447333f804403
Haythem97 <azzabihaythem@gmail.com>
2023-08-23 05:11:35 +0400

    server updated

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
 const path = require('path');
 const express = require('express');
+const cors = require('cors'); // Import the cors package
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
-const colors = require('colors');
-const dotenv = require('dotenv').config();
-
 
 connectDB();
 
 const app = express();
+
+// Use the cors middleware
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -19,15 +23,15 @@ app.use(`/api/goals/:goalId/objects`, require('./routes/objectRoutes'));
 
 // Serve frontend
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-  app.get('*', (req, res) =>
-      res.sendFile(
-          path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-      )
-  );
+    app.get('*', (req, res) =>
+        res.sendFile(
+            path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+        )
+    );
 } else {
-  app.get('/', (req, res) => res.send('Please set to production'));
+    app.get('/', (req, res) => res.send('Please set to production'));
 }
 
 app.use(errorHandler);
