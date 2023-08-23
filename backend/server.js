f38a50acf03727d8cb774cfd82f8891d49a6dd69
Haythem97 <azzabihaythem@gmail.com>
2023-08-23 05:08:53 +0400

    server updated

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
+const colors = require('colors');
+const dotenv = require('dotenv').config();
 
 
 connectDB();
