daa967704dae1672513a00a0ac4e5dfeb137b83c
Haythem97 <azzabihaythem@gmail.com>
2023-08-23 05:04:02 +0400

    server updated

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,13 @@
 const path = require('path');
 const express = require('express');
-const colors = require('colors');
-const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
-const fs = require("fs");
-const https = require("https");
-const cors = require('cors'); // Import the cors package
 const port = process.env.PORT || 5000;
+
 
 connectDB();
 
 const app = express();
-app.use(cors({
-  origin: 'http://localhost:3000' // Remplacez par l'origine de votre frontend
-}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -32,21 +25,9 @@ if (process.env.NODE_ENV === 'production') {
       )
   );
 } else {
-  app.get('/', (req, res) => res.send('/api/goals \n /api/objets'));
+  app.get('/', (req, res) => res.send('Please set to production'));
 }
 
 app.use(errorHandler);
 
-// Load SSL/TLS certificates
-const privateKey = fs.readFileSync('rootCAKey.pem', 'utf8');
-const certificate = fs.readFileSync('rootCACert.pem', 'utf8');
-
-const credentials = {
-  key: privateKey,
-  cert: certificate,
-};
-
-// Create HTTPS server
-const httpsServer = https.createServer(credentials, app);
-
-httpsServer.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`));
